d1a9c919a476ccdd78a81a60ae694e6c10e5b103
nekobako <nekobako.dev@outlook.com>
2017-11-28 15:24:25 +0900

    Specify AppUserModelId for PowerShell to avoid ToastNotifier restriction in Windows 10 Fall Creators Update

--- Capture/UI/Controls/Toast.cs
+++ Capture/UI/Controls/Toast.cs
@@ -41,7 +41,7 @@ namespace Capture.UI.Controls {
 
 		public Toast(XmlDocument content) {
 			this.Content = content;
-			this.notifier = ToastNotificationManager.CreateToastNotifier(typeof(Toast).FullName);
+			this.notifier = ToastNotificationManager.CreateToastNotifier(@"{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe");
 		}
 
 		public void Show() {
